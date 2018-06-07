c12aa6af85d5c8069d7c3ac8e8a9200261aec0ef
guidesc <romain.zhang91@mgail.com>
2018-06-07 05:09:38 +0200

    #4

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,16 @@
-var c = 1
-
+var c = 0
+var d = 1
+var source= 'https://github.com/guidesc/Petinkou/blob/master/audios/' + d + '.m4a?raw=true'
+console.log(source)
 Page({
   data: {
     curIdx: null,
     count:0,
-    source: 'https://github.com/guidesc/Petinkou/blob/master/audios/1.m4a?raw=true',
     listInfo: [
       {
         imgUrl: 'https://github.com/guidesc/Petinkou/blob/master/images/app1.gif?raw=true',
-        curUrl: 'https://github.com/guidesc/Petinkou/blob/master/images/app2-2.gif?raw=true',
-        tstUrl: '../../images/blue-lowerleft.png'
+        curUrl: 'https://github.com/guidesc/Petinkou/blob/master/images/app2.gif?raw=true',
+        tstUrl: 'https://github.com/guidesc/Petinkou/blob/master/images/app3.gif?raw=true'
 
       }
     ]
@@ -18,21 +19,23 @@ Page({
     
     this.setData({
       curIdx: e.currentTarget.dataset.current,
-      count: c,
-      source: 'https://github.com/guidesc/Petinkou/blob/master/audios/2.m4a?raw=true'
-    }),
+      count: c}),
     c = c + 1
-
+    d = d + 1
+    source = 'https://github.com/guidesc/Petinkou/blob/master/audios/' + d + '.m4a?raw=true'
+    
+    console.log(source)
+    backgroundAudioManager.src = source // 设置了 src 之后会自动播放
 
     //wx.pauseBackgroundAudio()
-      console.log(e)
-      console.log(this.data.curIdx) 
-      console.log(this.data.count)
+    //  console.log(e)
+    //  console.log(this.data.curIdx) 
+    //  console.log(this.data.count)
   }
 
 })
 
 const backgroundAudioManager = wx.getBackgroundAudioManager()
 
-backgroundAudioManager.src = 'https://github.com/guidesc/Petinkou/blob/master/audios/Swing%20Republic.m4a?raw=true' // 设置了 src 之后会自动播放
+backgroundAudioManager.src = source // 设置了 src 之后会自动播放
 
